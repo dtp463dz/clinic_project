b6814b759659c19137ccf52b184d773ad7d5d8e7
PhucDT <dinhthaiphuc463@gmail.com>
2025-04-11 02:34:02 +0700

    setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+// import './index.css'
+import App from './App.jsx';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import User from './components/User/User.jsx';
+import Admin from './components/Admin/Admin.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="users" element={<User />} />
+        <Route path="admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )

c44cc9ae4277a3a8244d74722de0f012f20dbb27
PhucDT <dinhthaiphuc463@gmail.com>
2025-04-12 09:50:00 +0700

    add layout

---
+++ src/Layout.jsx
@@ -0,0 +1,42 @@
+import App from './App.jsx';
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
+import User from './components/User/User.jsx';
+import Admin from './components/Admin/Admin.jsx';
+import Login from './components/Auth/Login.jsx';
+import Register from './components/Auth/Register.jsx';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const Layout = (props) => {
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<App />}>
+                    {/* <Route index element={<HomePage />} /> */}
+                    <Route path="users" element={<User />} />
+                </Route>
+                <Route path="admin" element={<Admin />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/register' element={<Register />} />
+
+            </Routes>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick={false}
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
+        </>
+    )
+}
+
+export default Layout;
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 // import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App.jsx';
 import { Provider } from 'react-redux';
 import { store } from './redux/store.jsx';
 
@@ -11,26 +10,15 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import User from './components/User/User.jsx';
-import Admin from './components/Admin/Admin.jsx';
-import Login from './components/Auth/Login.jsx';
-import Register from './components/Auth/Register.jsx';
+
+import Layout from './Layout.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />}>
-            {/* <Route index element={<HomePage />} /> */}
-            <Route path="users" element={<User />} />
-          </Route>
-          <Route path="admin" element={<Admin />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
+        <Layout />
 
-
-        </Routes>
       </BrowserRouter>
     </Provider>
 

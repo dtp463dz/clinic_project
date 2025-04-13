2c685bd27946c0112c6709526310dfad99d33308
PhucDT <dinhthaiphuc463@gmail.com>
2025-04-13 21:20:36 +0700

    using redux luu thong tin ng dung - chua fix phan login de hien thi nguoi dung

--- src/redux/reducer/rootReducer.jsx
+++ src/redux/reducer/rootReducer.jsx
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import counterReducer from './counterReducer.jsx';
+import userReducer from './userReducer.jsx';
 
 const rootReducer = combineReducers({
     counter: counterReducer,
+    user: userReducer,
 });
 
 export default rootReducer;
